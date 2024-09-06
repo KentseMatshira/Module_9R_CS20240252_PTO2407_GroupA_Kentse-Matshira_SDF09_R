9fd21242a3c542b16dcde8f0a25ed51cdd4901e4
KentseMatshira <kmatshira@yahoo.com>
2024-09-06 20:03:47 +0200

    updating javascript and modifying some css

--- index.js
+++ index.js
@@ -1,11 +1,79 @@
-// 1. Create two variables, firstCard and secondCard. 
+// 1. Create two variables, firstCard and secondCard.
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6
-let secondCard = 9
+let firstCard = 6;
+let secondCard = 9;
 
-let sum = firstCard + secondCard
+let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+let buttonEl = document.getElementById("start-btn");
+let player = {
+  name: "AC",
+  chips: 10000,
+};
 
-console.log(sum)
+playerEl.textContent = player.name + ": $" + player.chips;
+
+function getRandomCard() {
+  let randomNumber = Math.ceil(Math.random() * 13);
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
+
+function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+  buttonEl.disabled = true;
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Hit?";
+  } else if (sum === 21) {
+    message = "Blackjack!";
+    hasBlackJack = true;
+    newGame();
+  } else {
+    message = "Bust!";
+    isAlive = false;
+    newGame();
+  }
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
+}
+
+function newGame() {
+  buttonEl.disabled = false;
+}
